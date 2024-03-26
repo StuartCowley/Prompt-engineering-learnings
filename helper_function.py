6e54f4c91da2f545227020ba4f20188e28048dd4
StuartCowley <voidstalker@live.co.uk>
2024-03-26 14:18:30 +0000

    Add expanding plus temperature parameter

--- helper_function.py
+++ helper_function.py
@@ -9,10 +9,10 @@ client = OpenAI(api_key=os.environ.get("OPEN_AI_KEY"))
 #   {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
 #   {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
 # ]
-async def get_completion(messages, model="gpt-3.5-turbo"):
+async def get_completion(messages, temperature=0, model="gpt-3.5-turbo"):
     response = client.chat.completions.create(
         model=model,
         messages=messages,
-        temperature=0 # this is the degree of randomness of the model's output
+        temperature=temperature # this is the degree of randomness of the model's output. 0 = consistent, max 1 = more creative
     )
     return response.choices[0].message.content
